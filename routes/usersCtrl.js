49916106359008828bf55b32e8341791ee601507
TRAORE Daouda Erick <traoredaoudaerick@Air-de-TRAORE.lan>
2025-02-03 04:11:30 +0100

    add module async(npm)

--- routes/usersCtrl.js
+++ routes/usersCtrl.js
@@ -2,6 +2,7 @@
 const bcrypt = require('bcryptjs');
 const jwtUtils = require('../utils/jwt.utils');
 const models = require('../models');
+const asyncLib = require('async');
 
 const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 const PASSWORD_REGEX = /^(?=.*\d).{4,8}$/;
@@ -28,6 +29,53 @@ module.exports = {
         if (!PASSWORD_REGEX.test(password)) {
             return res.status(400).json({ 'error': 'Le mot de passe doit contenir entre 4 et 8 caractères et inclure au moins un chiffre' });
         }
+
+        asyncLib.waterfall([
+            function(done) {
+                models.User.findOne({
+                    attributes: ['email'],
+                    where: { email: email }
+                })
+                    .then(function(userFound) {
+                        done(null, userFound);
+                    })
+                    .catch(function(err) {
+                        return res.status(500).json({ 'error': 'Impossible de vérifier l\'utilisateur' });
+                    });
+            },
+            function(userFound, done) {
+                if (!userFound) {
+                    bcrypt.hash(password, 5, function(err, bcryptedPassword) {
+                        done(null, userFound, bcryptedPassword);
+                    });
+                } else {
+                    return res.status(409).json({ 'error': 'L\'utilisateur existe déjà' });
+                }
+            },
+            function(userFound, bcryptedPassword, done) {
+                const newUser = models.User.create({
+                    email: email,
+                    password: bcryptedPassword,
+                    bio: bio,
+                    isAdmin: 0
+                })
+                    .then(function(newUser) {
+                        done(newUser);
+                    })
+                    .catch(function(err) {
+                        return res.status(500).json({ 'error': 'Impossible d\'ajouter l\'utilisateur' });
+                    });
+            }
+        ], function(newUser) {
+            if (newUser) {
+                return res.status(201).json({
+                    'userId': newUser.id
+                });
+            } else {
+                return res.status(500).json({ 'error': 'Impossible d\'ajouter l\'utilisateur' });
+            }
+        });
+
         // Recherche de l'utilisateur dans la base de données
         models.User.findOne({
             attributes: ['email'],
@@ -97,5 +145,29 @@ module.exports = {
             })
             .catch(function(err) {
                 return res.status(500).json({ 'error': 'Impossible de vérifier l\'utilisateur' });
-    })
-}};
+        })
+    },
+    getUserProfile: function(req, res) {
+    // Récupération de l'en-tête d'autorisation
+    const headerAuth = req.headers['authorization'];
+    const userId = jwtUtils.getUserId(headerAuth);
+
+    if (userId < 0)
+        return res.status(400).json({ 'error': 'Token invalide' });
+
+    models.User.findOne({
+        attributes: ['id', 'email', 'bio'],
+        where: { id: userId }
+    }).then(function(user) {
+        if (user) {
+            res.status(201).json(user);
+        } else {
+            res.status(404).json({ 'error': 'Utilisateur non trouvé' });
+        }
+    }).catch(function(err) {
+        res.status(500).json({ 'error': 'Impossible de récupérer l\'utilisateur' });
+    });
+
+},
+
+};
